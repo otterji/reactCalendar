d1b093d94593e2c6f0329490306deda540855ada
노영지 <rkdudwl@naver.com>
2020-02-13 09:00:32 +0900

    COMPLETE 시 끝 당 && prepare subscribeList

--- Front-End/src/components/Calendar/Container.tsx
+++ Front-End/src/components/Calendar/Container.tsx
@@ -10,18 +10,20 @@ import {
 import { DateData, ModalData } from './_types/calendar';
 import { Calendar } from './Calendar';
 
-const Container: FunctionComponent<{}> = () => {
+const Container: FunctionComponent<{ data }> = props => {
+  const { sublist } = props;
+
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [viewDate, setViewDate] = useState(new Date());
   const [dataList, setDataList] = useState([] as DateData[]);
   const [toggleRender, setToggleRender] = useState(false);
   const [modalData, setModalData] = useState({} as ModalData);
-  
+
   // 보여지고 있는 달력의 데이터가 변경될 때마다 훅스가 실행
   // @ts-ignore
   useEffect(() => {
-    fetchData({viewDate, setDataList});
-  }, [viewDate, isModalOpen, toggleRender]);
+    fetchData({ sublist, viewDate, setDataList });
+  }, [sublist, viewDate, isModalOpen, toggleRender]);
 
   const showPrevMonthHandler = () => {
     const prevDate: Date = getPrevMonthDate(viewDate);
@@ -43,11 +45,11 @@ const Container: FunctionComponent<{}> = () => {
   return (
     <>
       <Title>
-        <Button style={{cursor: "pointer", outline: "none"}} onClick={showPrevMonthHandler}> &lt; </Button>
+        <Button style={{ cursor: "pointer", outline: "none" }} onClick={showPrevMonthHandler}> &lt; </Button>
         <h4 style={{ display: "inline-block" }}>
           {viewDate.getFullYear()}년 {viewDate.getMonth() + 1}월
         </h4>
-        <Button style={{cursor: "pointer", outline: "none"}} onClick={showNextMonthHandler}> &gt; </Button>
+        <Button style={{ cursor: "pointer", outline: "none" }} onClick={showNextMonthHandler}> &gt; </Button>
       </Title>
 
       <Calendar list={dataList} openModal={openModalHandler} reload={reloadHandler} />
@@ -58,7 +60,7 @@ const Container: FunctionComponent<{}> = () => {
           data={modalData}
           openModal={openModalHandler}
         />
-        ) : null}
+      ) : null}
     </>
   )
 }
